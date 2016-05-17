a409b8433ebb0af661b3c768752f630b3618cf8d
曲蛐蛐 <ququ@quququdemacbook-pro.local>
2016-05-17 12:18:24 +0800

    1、增加了name属性 2、增加了 打印 name 的方法

--- gitHubDemo/ViewController.h
+++ gitHubDemo/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) NSString * name;
+
 
 @end
 
--- gitHubDemo/ViewController.m
+++ gitHubDemo/ViewController.m
@@ -17,8 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+  
 }
 
+- (void)logName
+{
+    _name = @"蛐蛐";
+    NSLog(@"name == %@",_name);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
